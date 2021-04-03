afbc87bb4e8912eb3f7978c0009b90c522e3d6a4
Ochaart <mjhperez@gmail.com>
2021-04-03 14:25:01 -0700

    implemented router skeleton

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './Header/Header';
 import PatternPage from './PatternPage';
 import PatternCard from './PatternCard';
@@ -13,15 +14,19 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
+      <Router>
         <Header />
-        {/* <PatternPage /> */}
-        {/* <PatternCard cardWidth={252} imgSrc="https://static1.dmc.com/cache/p/a/pat0339_01_880x1322.jpg" />
-        <PatternCard cardWidth={252} imgSrc="https://static1.dmc.com/cache/p/a/pat14932_440x661.jpg" />
+        <Switch>
+          {/* <Route path="/users/:user_id" component={} */}
+          {/* <PatternPage /> */}
+          {/* <PatternCard cardWidth={252} imgSrc="https://static1.dmc.com/cache/p/a/pat0339_01_880x1322.jpg" />
+          <PatternCard cardWidth={252} imgSrc="https://static1.dmc.com/cache/p/a/pat14932_440x661.jpg" />
         <PatternCard cardWidth={252} imgSrc="https://i.pinimg.com/236x/da/49/82/da49825a87e5de376fa62a590d8b65c0.jpg" /> */}
-        Put your app here
-        { /* <AppHere /> */ }
-      </div>
+          Put your app here
+          { /* <AppHere /> */ }
+          {/* <Route path="/" component={} /> */}
+        </Switch>
+      </Router>
     );
   }
 }
