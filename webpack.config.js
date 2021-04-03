e62b07958e88ad3f2f6c8f6a734907fcedfea958
rfduong <ronnyduong@gmail.com>
2021-04-02 21:18:44 -0700

    Fixed css loader

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,17 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[path][name]__[local]--[hash:base64:5]',
+              },
+            },
+          },
+        ],
       },
     ],
   },
