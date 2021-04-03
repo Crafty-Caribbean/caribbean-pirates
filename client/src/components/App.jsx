61df4addc70ba4666f0933cacf84eb21a40ef161
Ochaart <mjhperez@gmail.com>
2021-04-03 14:15:35 -0700

    connected product card component cleaned up code added proptypes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header/Header';
 import PatternPage from './PatternPage';
 import PatternCard from './PatternCard';
+import UserPage from './UserPage/UserPage';
 
 class App extends React.Component {
   constructor(props) {
@@ -18,7 +19,7 @@ class App extends React.Component {
         {/* <PatternPage /> */}
         {/* <PatternCard /> */}
         Put your app here
-        { /* <AppHere /> */ }
+        <UserPage userId={1} />
       </div>
     );
   }
