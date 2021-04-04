0210f216eb839aaeae99a5d46307bfc134e509f7
Ika Ogeil <nahlaogeil@gmail.com>
2021-04-04 02:34:02 -0400

    adjusted styling for mini product cards and improved UI layout

--- client/src/components/UserPage/PatternList.jsx
+++ client/src/components/UserPage/PatternList.jsx
@@ -3,22 +3,30 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import TopArrow from './TopArrow';
 import DownArrow from './DownArrow';
-import styles from './userPage.module.css';
-import PatternCard from '../PatternCard/index';
+import styles from './PatternList.module.css';
+import PatternCard from './MiniPatternCard';
+import styled from 'styled-components';
 
+// const PatternStyle = styled.card`
+// height: 20vh;
+// `;
 const PatternList = ({ title, list }) => {
   console.log();
+
   return (
     <div>
       <div className={styles.title}>{title}</div>
-      <div className={styles.patternsList}>
-        {list.map((pattern) => <PatternCard info={pattern} key={pattern.id} imgSrc="https://static1.dmc.com/cache/p/a/pat0339_01_880x1322.jpg" progress={pattern.progress} title={title} />)}
-        <TopArrow />
-        <DownArrow />
+      <TopArrow />
+      <div id="patternList" className={styles.patternList}>
+        {list.map((pattern) =>
+          <PatternCard info={pattern} key={pattern.id} imgSrc="https://static1.dmc.com/cache/p/a/pat0339_01_880x1322.jpg" progress={pattern.progress} title={title} />
+        )}
       </div>
+      <DownArrow />
     </div>
   );
 };
+
 
 export default PatternList;
 
