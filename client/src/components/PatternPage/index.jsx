4f84370e278c244a353348054338e52731143f21
Phillip Ho <philasan@gmail.com>
2021-04-05 12:47:59 -0700

    create modal

--- client/src/components/PatternPage/index.jsx
+++ client/src/components/PatternPage/index.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import Login from '../Login';
+import AppModal from '../AppModal';
+
 import styles from './PatternPage.css';
 import PatternSummary from './PatternSummary';
 
 class PatternPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      showModal: false,
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+  }
+
+  toggleModal(e) {
+    e.preventDefault();
+
+    const { showModal } = this.state;
+
+    this.setState({
+      showModal: !showModal,
+    });
   }
 
   render() {
-    // console.log(styles);
+    const { showModal } = this.state;
 
     return (
       <div className={styles.patternPage}>
@@ -19,7 +36,18 @@ class PatternPage extends React.Component {
             <div className={styles.patternSummary}><PatternSummary /></div>
           </div>
         </div>
-        <div className={styles.relatedPatterns}>Related Patterns</div>
+        <div className={styles.relatedPatterns}>
+          Related Patterns
+          <button type="button" onClick={this.toggleModal}>Modal</button>
+        </div>
+        {
+          showModal
+          && (
+            <AppModal ref={this.registerModal} outsideClickHandler={this.toggleModal}>
+              <Login />
+            </AppModal>
+          )
+        }
       </div>
     );
   }
