f3b5cb39f47d6f06e642497e476c51ffe667eb84
Benjamin Wu <benngfour@gmail.com>
2021-04-05 13:56:24 -0700

    clean up before submit PR.

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '/../client/dist')));
-// app.use('/*', express.static(path.join(__dirname, '/../client/dist')));
+app.use('/*', express.static(path.join(__dirname, '/../client/dist')));
 
 app.use('/', router);
 
