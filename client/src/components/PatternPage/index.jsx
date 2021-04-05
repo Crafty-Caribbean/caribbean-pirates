09916d2fe442e1a9127d5e45d4cbfb2c37aeee47
Phillip Ho <philasan@gmail.com>
2021-04-05 13:13:00 -0700

    remove unnecessary ref

--- client/src/components/PatternPage/index.jsx
+++ client/src/components/PatternPage/index.jsx
@@ -43,7 +43,7 @@ class PatternPage extends React.Component {
         {
           showModal
           && (
-            <AppModal ref={this.registerModal} outsideClickHandler={this.toggleModal}>
+            <AppModal outsideClickHandler={this.toggleModal}>
               <Login />
             </AppModal>
           )
