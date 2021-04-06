70f6dd91b4f5cba73e9e2bbca16445a4520b50f1
Benjamin Wu <benngfour@gmail.com>
2021-04-05 17:14:57 -0700

    Implemented update project progress PUT request handler

--- db/models/user_projects.js
+++ db/models/user_projects.js
@@ -19,4 +19,42 @@ module.exports = {
       });
     });
   },
+
+  updateProgress(userId, patternId, progress, callback) {
+    const query = {
+      text: 'UPDATE public.user_projects SET progress=$1 WHERE user_id=$2 AND pattern_id=$3;',
+      values: [progress, userId, patternId],
+    };
+    db.connect((err, client, release) => {
+      if (err) {
+        console.error('Error updating project progress', err.stack);
+      }
+      client.query(query, (error, results) => {
+        release();
+        if (error) {
+          callback(error.stack);
+        }
+        callback(null, results);
+      });
+    });
+  },
+
+  updateCompletedTime(userId, patternId, callback) {
+    const query = {
+      text: 'UPDATE public.user_projects SET completed_at=NOW() WHERE user_id=$1 AND pattern_id=$2;',
+      values: [userId, patternId],
+    };
+    db.connect((err, client, release) => {
+      if (err) {
+        console.error('Error updating project complete time', err.stack);
+      }
+      client.query(query, (error, results) => {
+        release();
+        if (error) {
+          callback(error.stack);
+        }
+        callback(null, results);
+      });
+    });
+  },
 };
--- server/controllers/user_projects.js
+++ server/controllers/user_projects.js
@@ -1,14 +1,46 @@
 const userProjectsModels = require('../../db/models/user_projects');
 
 module.exports = {
-  userAddProject(req, res) {
-    userProjectsModels.addProject(req.params.user_id,
-      req.body.pattern_id, (err) => {
+  addProject(req, res) {
+    userProjectsModels.addProject(
+      req.params.user_id,
+      req.body.pattern_id,
+      (err) => {
         if (err) {
           console.error(err);
           res.status(400).send('Failed adding project');
         }
         res.status(201).send(`Added project into user ${req.params.user_id}`);
-      });
+      },
+    );
+  },
+
+  updateProjectProgress(req, res) {
+    userProjectsModels.updateProgress(
+      Number(req.params.user_id),
+      Number(req.params.pattern_id),
+      Number(req.body.progress),
+      (err) => {
+        if (err) {
+          console.error(err);
+          res.status(400).send('Failed updating progress', err);
+        }
+        if (Number(req.body.progress) < 100) {
+          res.status(201).send('Project progress updated!');
+        } else {
+          userProjectsModels.updateCompletedTime(
+            Number(req.params.user_id),
+            Number(req.params.pattern_id),
+            (error) => {
+              if (error) {
+                console.error(error);
+                res.status(400).send('Failed updating complete time', error);
+              }
+              res.status(201).send('Project completed!');
+            },
+          );
+        }
+      },
+    );
   },
 };
