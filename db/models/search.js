a846a884ba87fee86766a6db56c6941efb044c86
Benjamin Wu <benngfour@gmail.com>
2021-04-06 17:18:38 -0700

    Impletmented model and controller for searching

--- db/models/search.js
+++ db/models/search.js
@@ -1,7 +1,31 @@
 const db = require('../index.js');
 
 module.exports = {
-  getResult(keywords) {
-
+  getResults(keyword, callback) {
+    const matchKeyword = `%${keyword}%`;
+    const query = {
+      text: `SELECT json_build_object(
+        'users', (SELECT COALESCE(json_agg(users), '[]'::json)
+                  FROM (SELECT id, username
+                  FROM users
+                  WHERE username LIKE $1) AS users),
+        'patterns', (SELECT COALESCE(json_agg(patterns), '[]'::json)
+                     FROM (SELECT id, title
+                     FROM patterns
+                     WHERE title LIKE $2) AS patterns)) AS results`,
+      values: [matchKeyword, matchKeyword],
+    };
+    db.connect((err, client, release) => {
+      if (err) {
+        console.error('Error getting data', err.stack);
+      }
+      client.query(query, (error, result) => {
+        release();
+        if (error) {
+          callback(err.stack);
+        }
+        callback(null, result);
+      });
+    });
   },
 };
--- server/controllers/search.js
+++ server/controllers/search.js
@@ -2,6 +2,12 @@ const searchModels = require('../../db/models/search');
 
 module.exports = {
   getSearchResult(req, res) {
-
+    searchModels.getResults(req.query.keyword, (err, results) => {
+      if (err) {
+        console.error(err);
+        res.status(400).send('Failed getting results');
+      }
+      res.status(200).send(results.rows[0].results);
+    });
   },
 };
