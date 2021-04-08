de1965f52564e1be25ca8e015172fc9d4381d384
Gordon-Chow <Roarekknight@gmail.com>
2021-04-07 21:34:46 -0700

    Refactored to a functional component

--- client/src/components/PatternPage/FavoritesButton.jsx
+++ client/src/components/PatternPage/FavoritesButton.jsx
@@ -1,25 +1,18 @@
 import React from 'react';
 import { HiHeart, HiOutlineHeart } from 'react-icons/hi';
-import styles from './FavoritesButton.css'
+import styles from './FavoritesButton.css';
 
-class FavoritesButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { };
-  }
+const FavoritesButton = (props) => {
+  const { isLiked, size, handleClick } = props;
+  const buttonState = isLiked
+    ? <HiHeart className={styles.test} size={size} color="pink" />
+    : <HiOutlineHeart size={size} color="gray" />;
 
-  render() {
-    const { isLiked, size, handleClick } = this.props;
-    const anythingyouwant = isLiked
-      ? <HiHeart className={styles.test} size={size} color="pink" />
-      : <HiOutlineHeart size={size} color="gray" />;
-
-    return (
-      <button className={styles.FavoritesButton} onClick={handleClick} type="button">
-        {anythingyouwant}
-      </button>
-    );
-  }
-}
+  return (
+    <button className={styles.FavoritesButton} onClick={handleClick} type="button">
+      {buttonState}
+    </button>
+  );
+};
 
 export default FavoritesButton;
