bcb6216b6f1f4ce23b319e2a8fcce5548c172f28
rfduong <ronnyduong@gmail.com>
2021-04-08 22:30:17 -0700

    Testing around with jwt cookies

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import decode from 'jwt-decode';
+import Cookies from 'js-cookie';
 import Header from './Header/Header';
 import PatternPage from './PatternPage';
 import PatternCard from './PatternCard';
@@ -23,10 +24,24 @@ class App extends React.Component {
 
   componentDidMount() {
     this.fetchHomeData();
+    console.log(Cookies.get('shortlyid'));
+    setTimeout(() => console.log(document.cookie), 2000);
+  }
+
+  componentDidUpdate() {
+    console.log(Cookies.get('shortlyid'));
+    console.log(Cookies.get('token'));
   }
 
   login(token) {
     const { username, user_id } = decode(token);
+    const expiresIn = 1/24;
+    Cookies.set('token', token, {
+      expires: expiresIn,
+    });
+    document.cookie = `token=${token}`;
+    console.log(Cookies.get('token'));
+    console.log(document.cookie);
     this.setState({
       isLoggedIn: true,
       token,
@@ -41,6 +56,7 @@ class App extends React.Component {
     axios.get('/api/patterns')
       .then((response) => {
         const { data } = response;
+        console.log(Cookies.get('token'));
         this.setState({ data });
       })
       .catch((error) => {
