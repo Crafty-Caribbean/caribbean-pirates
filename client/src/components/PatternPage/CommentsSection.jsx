648628ac4e4fcb843e9f2fd1b36983c2b39dfef5
Monica Bui <monicabbui@gmail.com>
2021-04-08 17:04:50 -0700

    Fix bug

--- client/src/components/PatternPage/CommentsSection.jsx
+++ client/src/components/PatternPage/CommentsSection.jsx
@@ -6,38 +6,20 @@ import CommentTiles from './CommentTiles.jsx'
 class CommentsSection extends React.Component {
   constructor(props) {
     super(props);
+    const { comments } = this.props;
     this.state = {
       commentText: '',
-      // comments: this.props.comments,
-      comments: [
-        {
-          id: 1, username: 'Monica', content: 'Amazing work!', created_at: '2021-04-07T09:57:29.103Z',
-        },
-        {
-          id: 2, username: 'Ben', content: 'Loved this pattern', created_at: '2021-04-07T09:58:03.547Z',
-        },
-        {
-          id: 3, username: 'Ronny', content: 'Great details', created_at: '2021-04-07T09:58:27.559Z',
-        },
-        {
-          id: 4, username: 'Gordon', content: 'Time to learn something new!', created_at: '2021-04-07T09:58:57.185Z',
-        },
-        {
-          id: 5, username: 'Phil', content: 'I want to make this one for my dog~~', created_at: '2021-04-07T09:59:30.909Z',
-        },
-        {
-          id: 6, username: 'Mikey', content: 'Oh yeah, I need this!', created_at: '2021-04-07T10:06:00.961Z',
-        },
-        {
-          id: 7, username: 'Ika', content: 'Presented to you by the amazing Crafty Caribbean', created_at: '2021-04-07T10:29:39.246Z',
-        },
-      ],
+      comments: comments,
     };
     this.handleCommentInput = this.handleCommentInput.bind(this);
     this.handleReset = this.handleReset.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.getComments = this.getComments.bind(this);
     this.addComments = this.addComments.bind(this);
+  }
+
+  componentDidMount() {
+    this.getComments();
   }
 
   handleCommentInput(event) {
